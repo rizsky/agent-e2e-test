2a1445613bc5cb0d8831a14b320822f1dc30e493
rizsky <muhammad.rizsky@ralali.com>
2020-01-02 21:06:59 +0700

    refactoring the test runner script and env

--- cypress/config/cypress-config-resolver.js
+++ cypress/config/cypress-config-resolver.js
@@ -3,8 +3,9 @@ const path = require('path')
 
 const cypressConfigResolverByFile = (filename) => {
   const pathToConfigFile = path.resolve(__dirname, `${filename}.json`)
+  console.log(pathToConfigFile)
   return fs.readJsonSync(pathToConfigFile)
 }
-const cypressConfigResolver = () => cypressConfigResolverByFile(process.env.CYPRESS_ENV || 'local')
+const cypressConfigResolver = () => cypressConfigResolverByFile(process.env.ENV_STAGE || 'development')
 
 module.exports.cypressConfigResolver = cypressConfigResolver
